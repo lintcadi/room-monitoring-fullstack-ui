bc198d86805a5d40fe87b86bda54180a3cc2e0be
lintcadi <lintangcahyo207@gmail.com>
2026-09-20 16:48:50 +0800

    add telemetry history endpoint

--- backend/src/backend/routers/telemetry.py
+++ backend/src/backend/routers/telemetry.py
@@ -1,6 +1,7 @@
-from datetime import datetime
+from datetime import datetime, UTC
+from typing import Annotated
 
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Query
 from pydantic import BaseModel
 
 from backend.database import pool
@@ -20,6 +21,14 @@ class TelemetryValue(BaseModel):
 class LatestTelemetryResponse(BaseModel):
     observed_at: datetime
     readings: list[TelemetryValue]
+
+class HistoricalTelemetryResponse(BaseModel):
+    observed_at: datetime
+    tag_key: str
+    name: str
+    unit: str | None
+    value: float
+    quality: int
 
 @router.get("/latest", response_model=LatestTelemetryResponse)
 async def get_latest_telemetry() -> LatestTelemetryResponse:
@@ -69,3 +78,68 @@ async def get_latest_telemetry() -> LatestTelemetryResponse:
         observed_at=observed_at,
         readings=readings
     )
+
+@router.get("/history", response_model=list[HistoricalTelemetryResponse])
+async def get_telemetry_history(
+        start: datetime,
+        end: datetime,
+        tags: Annotated[list[str] | None, Query()] = None
+) -> list[HistoricalTelemetryResponse]:
+    if start.tzinfo is None or end.tzinfo is None:
+        raise HTTPException(
+            status_code=400,
+            detail="Both start and end datetimes are null",
+        )
+
+    start = start.astimezone(UTC)
+    end = end.astimezone(UTC)
+
+    if start > end:
+        raise HTTPException(
+            status_code=400,
+            detail="Start datetime must be less than end datetime",
+        )
+
+    query = """
+        SELECT
+            tr.observed_at,
+            t.tag_key,
+            t.name,
+            t.unit,
+            tr.value,
+            tr.quality
+        FROM telemetry_readings AS tr
+        JOIN tags as t 
+            ON t.id = tr.tag_id
+        WHERE tr.observed_at >= %s
+            AND tr.observed_at <= %s
+    """
+
+    params: list[datetime | list[str]] = [start, end]
+
+    if tags:
+        query += """
+            AND t.tag_key = ANY(%s)
+        """
+        params.append(tags) # no
+
+    query += """
+        ORDER BY tr.observed_at, t.id
+    """
+
+    async with pool.connection() as conn:
+        async with conn.cursor() as cursor:
+            await cursor.execute(query, params)
+            rows = await cursor.fetchall()
+
+    return [
+        HistoricalTelemetryResponse(
+            observed_at=row[0],
+            tag_key=row[1],
+            name=row[2],
+            unit=row[3],
+            value=row[4],
+            quality=row[5],
+        )
+        for row in rows
+    ]
